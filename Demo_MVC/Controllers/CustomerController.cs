13c880ba62c9e7ae529f601a0d213aa6ee27e504
lovefamilyviet203 <huyviet181@gmail.com>
2024-03-22 07:59:39 +0700

    Add project files.

---
+++ Demo_MVC/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Customer()
+        {
+            return View();
+        }
+    }
+}
